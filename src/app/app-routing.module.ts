34e55786a36682c394fa03f35ebd8a47b8427728
Daniel Vanegas <danieru1313@gmail.com>
2020-05-05 03:53:14 -0500

    Move create-question component to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ItemsComponent} from './components/items/items.component';
-import {CreateComponent} from './components/create/create.component';
 
 
 const routes: Routes = [
   {path: '',component:ItemsComponent},
   {path: 'items',component:ItemsComponent},
-  {path: 'create',component:CreateComponent}
 ];
 
 @NgModule({
