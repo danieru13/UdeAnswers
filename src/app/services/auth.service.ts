33c5b5f9d64cd07fa684476ecf1328dfb26a97be
Daniel Vanegas <danieru1313@gmail.com>
2020-05-09 18:02:03 -0500

    Adding guards and routes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -42,7 +42,7 @@ export class AuthService {
 
    async signOut() {
      await this.afAuth.signOut();
-     return this.router.navigate(['/']);
+     return this.router.navigate(['/login']);
    }
 
    private updateUserData({ uid, email, displayName, photoURL }: User) {
@@ -57,6 +57,12 @@ export class AuthService {
      };
 
      return userRef.set(data, { merge: true });
+   }
+
+   login() {
+     this.googleSignin().then(()=>{
+       this.router.navigate(['/']);
+     })
    }
    
 }
