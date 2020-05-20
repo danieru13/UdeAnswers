c334c514e8a45e8120afce31855778503720bc34
Daniel Vanegas <danieru1313@gmail.com>
2020-05-20 17:23:37 -0500

    Add route for question by id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import {QuestionListComponent} from './components/question-list/question-list.co
 import { AuthGuard } from './services/auth.guard';
 import { UserLoginComponent } from './components/user-login/user-login.component';
 import { QuestionSearchComponent } from './components/question-search/question-search.component';
+import { QuestionComponent } from './components/question/question.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/questions', pathMatch: 'full'},
-  {path: 'questions',component:QuestionListComponent, canActivate: [AuthGuard]},
-  {path: 'search/:query',component:QuestionSearchComponent},
+  {path: 'questions', component:QuestionListComponent, canActivate: [AuthGuard]},
+  {path: 'question/:id', component: QuestionComponent},
+  {path: 'search/:query', component:QuestionSearchComponent},
   {path: 'login', component: UserLoginComponent}
 
 ];
